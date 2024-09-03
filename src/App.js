ef855ee993815e0a5a873c1d6b0239f8babe9452
Jeevan099 <jeevan09offical@gmail.com>
2024-09-03 21:13:18 +0530

    change of style

--- src/App.js
+++ src/App.js
@@ -1,159 +1,90 @@
 import "./styles.css";
 
+import { useEffect } from "react";
+
 export default function App() {
   function fok() {
     var j = document.getElementById("arr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    }
   }
 
   function kof() {
     var j = document.getElementById("arr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    }
   }
 
   function gok() {
     let j = document.getElementById("brr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    }
   }
 
   function kog() {
     let j = document.getElementById("brr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    }
   }
 
   function hok() {
     let j = document.getElementById("crr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/premium/png-64-thumb/chevron-arrow-3883460-3231250.png)";
+    }
   }
 
   function koh() {
     let j = document.getElementById("crr");
-    j.style.backgroundImage =
-      "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    if (j) {
+      j.style.backgroundImage =
+        "url(https://cdn.iconscout.com/icon/free/png-64/right-arrow-1438234-1216195.png)";
+    }
   }
-
-  window.onscroll = function () {
-    jet();
-  };
 
   function jet() {
     var ilake = document.getElementById("head");
-    ilake.style.top = "0px";
-    ilake.style.position = "sticky";
+    if (ilake) {
+      ilake.style.top = "0px";
+      ilake.style.position = "sticky";
+    }
   }
 
-  window.addEventListener("scroll", () => {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var wnd = window.innerHeight;
-      var rtop = reveals[i].getBoundingClientRect().top;
-      var rpoint = 100;
+  useEffect(() => {
+    window.onscroll = function () {
+      jet();
+    };
 
-      if (rtop < wnd - rpoint) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
+    window.addEventListener("scroll", () => {
+      var reveals = document.querySelectorAll(".reveal");
+      for (var i = 0; i < reveals.length; i++) {
+        var wnd = window.innerHeight;
+        var rtop = reveals[i].getBoundingClientRect().top;
+        var rpoint = 100;
+
+        if (rtop < wnd - rpoint) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
       }
-    }
-  });
+    });
+
+    return () => {
+      window.removeEventListener("scroll", jet);
+    };
+  }, []);
   return (
     <div classNameName="App">
-      {/* { <nav>
-        <div>
-          <a href="mailto: karan.kumar@ecerasystem.com">
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-64/email-letter-envelope-message-38065.png"
-              alt="G-mail"
-            />
-            <span
-              style={{
-                color: "white",
-                fontFamily:
-                  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
-              }}
-            >
-              karan.kumar@ecerasystem.com
-            </span>
-          </a>
-          <a href="tel: +91 123456789">
-            <img
-              src="https://cdn.iconscout.com/icon/premium/png-64-thumb/telephone-41-117249.png"
-              alt=""
-            />
-            <span
-              style={{
-                color: "white",
-                fontFamily:
-                  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
-              }}
-            >
-              +91 123456789
-            </span>
-          </a>
-        </div>
-        <div>
-          <a href={{}}>
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-64/instagram-1868978-1583142.png"
-              alt=""
-            />
-          </a>
-          <a href={{}}>
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-64/facebook-logo-3771014-3147631.png"
-              alt=""
-            />
-          </a>
-          <a href={{}}>
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-64/linkedin-162-498418.png"
-              alt=""
-            />
-          </a>
-          <a href={{}}>
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-64/telegram-2752057-2284874.png"
-              alt=""
-            />
-          </a>
-        </div>
-      </nav> } */}
-      { <header id="head">
-        <a href={{}}>
-          <span>
-            <img
-              src=""
-              alt="Logo"
-            />
-          </span>
-          <span id="c_name">Good Green Care Center</span>
-        </a>
-        <ul>
-          <li>
-            <a href={{}}>Home</a>
-          </li>
-          <li>
-            <a href={{}} onMouseOver={gok} onMouseLeave={kog}>
-              Register/Sign In
-            </a>
-            <div id="brr"></div>
-          </li>
-          <li>
-            <a href={{}} onMouseOver={fok} onMouseLeave={kof}>
-              Services
-            </a>
-            <div id="arr"></div>
-          </li>
-          <li>
-            <a href={{}}>Contact Us</a>
-          </li>
-        </ul>
-      </header> }
       <main>
         <div id="front">
           <h1 style={{ textAlign: "center" }}>Jimmy's Rehabilitation Center</h1>
@@ -387,81 +318,6 @@ export default function App() {
         </div>
       </main>
 
-      {/* <footer
-        style={{ display: "flex", "justify-content": "space-around" }}
-        id="foote"
-      >
-        <ul>
-          <li>
-            <a href={{}}>About Us</a>
-          </li>
-          <li>
-            <a href={{}}>Carrers</a>
-          </li>
-          <li>
-            <a href={{}}>Blogs</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href={{}}>Training</a>
-          </li>
-          <li>
-            <a href={{}}>FAQs</a>
-          </li>
-        </ul>
-        <div>
-          <h2>Conatact Us</h2>
-          <span>
-            <a href={{}}>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-64/instagram-188-498425.png"
-                alt=" "
-              />
-            </a>
-            <a href={{}}>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-64/facebook-262-721949.png"
-                alt=" "
-              />
-            </a>
-            <a href={{}}>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-64/whatsapp-43-189795.png"
-                alt=" "
-              />
-            </a>
-          </span>
-          <span>
-            <a href={{}}>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-64/telegram-2752057-2284874.png"
-                alt=" "
-              />
-            </a>
-            <a href={{}}>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-64/linkedin-162-498418.png"
-                alt=" "
-              />
-            </a>
-          </span>
-          <a
-            href="tel: 123456789"
-            style={{
-              color: "white",
-              fontSize: "1.3em",
-              letterSpacing: "2px",
-              textAlign: "center",
-              textDecoration: "none",
-              fontWeight: "bolder",
-              marginTop: "20px"
-            }}
-          >
-            Telephone No: +91 232345553
-          </a>
-        </div>
-      </footer> */}
       <p
         style={{
           color: "white",
